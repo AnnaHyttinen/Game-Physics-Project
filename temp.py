a685ab9658f53e1d44d4fcc51f4f87f9e393672f
AnnaHyttinen <93588412+AnnaHyttinen@users.noreply.github.com>
2024-04-23 14:24:55 +0300

    Initial values for triangles added
    Also starting points established accordingly.

--- temp.py
+++ temp.py
@@ -3,35 +3,43 @@
 import matplotlib.pyplot as plt
 import math
 
-# edge points and centers of mass for triangles 
-# A and B introduced in various ways: 
+# reference points introduced
 
-p1A = (2.0, 1.0)
-p2A = (-1.0, 1.0)
-p3A = (-1.0, -2.0)
+p1Ai = (2.0, 1.0)
+p2Ai = (-1.0, 1.0)
+p3Ai = (-1.0, -2.0)
 
-cmA = (0.0, 0.0)
+p1Bi = (1.0, 0.0)
+p2Bi = (-1.0, 1.0)
+p3Bi = (0.0, -1.0)
+
+cmi = (0.0, 0.0)
+cmi = 0.0 
+cmi = 0.0
+
+# starting points for centers of mass introduced (various ways)
+
+cmA = (0.0, 2.0)
 cmAx = 0.0 
-cmAy = 0.0
-
-p1B = (5.0, 3.0)
-p2B = (3.0, 4.0)
-p3B = (4.0, 2.0)
+cmAy = 2.0
 
 cmB = (4.0, 3.0)
 cmBx = 4.0 
 cmBy = 3.0
 
-# general variables needed for calculations: 
+# general variables needed for calculations
 
 time = 0.0
 g = 9.81
 dt = 0.1
 y = 1.0 # greek alphabet that looks a bit like y
-w = 1.0 # greek alphabet that looks a bit like w
+w = 1.0 # greek alphabet that looks a bit like w (omega)
 e = 0.8
+m = 1.0 # just one mass, equal to both triangles
+IA = 1.5
+IB = 0.5
 
-# introducing centers of mass for triangles A and B
+# introducing components of velocity and tracks centers of masses
 
 VxcmA = 2.0
 VycmA = 7.0
@@ -43,13 +51,21 @@ VycmB = 3.0
 dotsBx = [cmBx]
 dotsBy = [cmBy]
 
-# wrapping up information about the triangles A and B
+# setting up the triangles A and B
+
+p1A = (p1Ai[0] + cmA[0], p1Ai[1] + cmA[1])
+p2A = (p2Ai[0] + cmA[0], p2Ai[1] + cmA[1])
+p3A = (p3Ai[0] + cmA[0], p3Ai[1] + cmA[1])
 
 A = (p1A, p2A, p3A, p1A) # to draw lines from beginning to end
 Ax = [p[0] for p in A]
 Ay = [p[1] for p in A]
 cornersAx = [Ax] # for the purposes of printing the triangles
 cornersAy = [Ay]
+
+p1B = (p1Bi[0] + cmB[0], p1Bi[1] + cmB[1])
+p2B = (p2Ai[0] + cmB[0], p2Bi[1] + cmB[1])
+p3B = (p3Bi[0] + cmB[0], p3Bi[1] + cmB[1])
 
 B = (p1B, p2B, p3B, p1B)
 Bx = [p[0] for p in B]
