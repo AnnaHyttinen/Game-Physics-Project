ed3b61ee157bc74ab3053d84967a426ca1ccdd09
AnnaHyttinen <93588412+AnnaHyttinen@users.noreply.github.com>
2024-04-23 11:20:14 +0300

    Ax is no longer a tuple, but a list
    List is easier to manipulate than a tuple

--- temp.py
+++ temp.py
@@ -45,13 +45,15 @@ dotsBy = [cmBy]
 
 # wrapping up information about the triangles A and B
 
-A = (p1A, p2A, p3A, p1A) # this helps to draw lines from beginning to end
-Ax, Ay = zip(*A) # creates lists of corner dots's x and y elements
-cornersAx = [Ax] # do we really even need these? well, they are in use now
+A = (p1A, p2A, p3A, p1A) # to draw lines from beginning to end
+Ax = [p[0] for p in A]
+Ay = [p[1] for p in A]
+cornersAx = [Ax] # for the purposes of printing the triangles
 cornersAy = [Ay]
 
 B = (p1B, p2B, p3B, p1B)
-Bx, By = zip(*B)
+Bx = [p[0] for p in B]
+By = [p[1] for p in B]
 cornersBx = [Bx]
 cornersBy = [By]
 
@@ -70,13 +72,18 @@ while (time < 3.0):
     dotsAy.append(dotsAy[-1] + VycmA * dt)
     
     # calculating and updating the corners
-    for i in Ax:
-        i = i*math.cos(y) - i*math.sin(y)
-    for i in Ay:
-        i = i*math.sin(y) + i*math.cos(y)
+    """Ax[0] = Ax[0]*math.cos(y) - Ax[0]*math.sin(y)
+    Ax[1] = Ax[1]*math.cos(y) - Ax[1]*math.sin(y)
+    Ax[2] = Ax[2]*math.cos(y) - Ax[2]*math.sin(y)
+    Ax[3] = Ax[0]
+
+    Ay[0] = Ay[0]*math.sin(y) + Ay[0]*math.cos(y)
+    Ay[1] = Ay[1]*math.sin(y) + Ay[1]*math.cos(y)
+    Ay[2] = Ay[2]*math.sin(y) + Ay[2]*math.cos(y)
+    Ax[3] = Ax[0]"""
     plt.plot(Ax, Ay)
     
-    # Same for triangle B:
+    # Same for triangle B
     
     if(dotsBy[-1] < 0 and VycmB < 0): # collision check for B
         VycmB = -e * VycmB
@@ -87,11 +94,16 @@ while (time < 3.0):
     cmBy = cmBy + VycmB * dt
     dotsBx.append(dotsBx[-1] + VxcmB * dt)
     dotsBy.append(dotsBy[-1] + VycmB * dt)
-    
-    for i in Bx:
-        i = i*math.cos(y) - i*math.sin(y)
-    for i in By:
-        i = i*math.sin(y) + i*math.cos(y)
+
+    """Bx[0] = Bx[0]*math.cos(y) - Bx[0]*math.sin(y)
+    Bx[1] = Bx[1]*math.cos(y) - Bx[1]*math.sin(y)
+    Bx[2] = Bx[2]*math.cos(y) - Bx[2]*math.sin(y)
+    Bx[3] = Bx[0]
+
+    By[0] = By[0]*math.sin(y) + By[0]*math.cos(y)
+    By[1] = By[1]*math.sin(y) + By[1]*math.cos(y)
+    By[2] = By[2]*math.sin(y) + By[2]*math.cos(y)
+    Bx[3] = Bx[0]"""
     plt.plot(Bx, By)
     
     # updating the other variables: 
@@ -101,4 +113,5 @@ while (time < 3.0):
 # Plotting the lists and showing it
 plt.plot(dotsAx, dotsAy, 'o')
 plt.plot(dotsBx, dotsBy,'x')
+plt.gca().set_aspect('equal')
 plt.show()
