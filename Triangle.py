3475c25d5ccc91628fb9d03b967d980f4b74b30e
immiren <88034638+immiren@users.noreply.github.com>
2024-05-11 23:29:47 +0300

    worked on classes

--- Triangle.py
+++ Triangle.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import math
+
 class Triangle:
     def __init__(self, cm, points, angDispl, angVel, mass, I):
         self.cm = cm
@@ -7,16 +10,33 @@ class Triangle:
         self.mass = mass
         self.I = I
         print(f"angDispl = {angDispl}, angVel = {angVel}")
-        for point in points:
+        for point in self.points:
             point.printPoint()
-        cm.printPoint()
+        self.cm.printPoint()
+    def updateCorners(self):
+        for i in self.points:
+            i.updatePoint(self.angDispl, self.cm)
+    def updateCM(self, dt):
+        self.cm.updatePoint(dt)
+    def updateCMVelocity(self, g, dt):
+        self.cm.updateNoCollisionVelocity(g, dt)
+    def plotPoints(self):
+        plt.plot([self.points[0].x,self.points[1].x,self.points[2].x,self.points[0].x,], [self.points[0].y,self.points[1].y,self.points[2].y,self.points[0].y,])
+        #print([self.points[0].x,self.points[1].x,self.points[2].x,self.points[0].x,])
+        #print([self.points[0].y,self.points[1].y,self.points[2].y,self.points[0].y,])
+        
         
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+    def updatePoint(self, angDispl, cm):
+        self.x = self.x*math.cos(angDispl) - self.y*math.sin(angDispl) + cm.x
+        self.y = self.x*math.sin(angDispl) + self.y*math.cos(angDispl) + cm.y
     def printPoint(self):
         print(f"Location: {self.x}, {self.y}")
+    def plotPoint(self):
+        plt.plot(self.x, self.y)
     
 class CenterOfMass(Point):
     def __init__(self,x,y, vx, vy):
@@ -26,3 +46,9 @@ class CenterOfMass(Point):
     def printPoint(self):
         super().printPoint()
         print(f"Velocity: {self.vx}i + {self.vy}j")
+    def updatePoint(self, dt):
+        self.x = self.x + self.vx * dt
+        self.y = self.y + self.vy * dt
+        #self.printPoint()
+    def updateNoCollisionVelocity(self, g, dt):
+        self.vy = self.vy - g * dt
--- temp.py
+++ temp.py
@@ -2,7 +2,66 @@
 
 import matplotlib.pyplot as plt
 import math
-from Triangle import Triangle, Point, CenterOfMass
+#from Triangle import Triangle, Point, CenterOfMass
+
+class Triangle:
+    def __init__(self, cm, points, angDispl, angVel, mass, I):
+        self.cm = cm
+        self.points = points
+        self.angDispl = angDispl
+        self.angVel = angVel
+        self.mass = mass
+        self.I = I
+        print(f"angDispl = {angDispl}, angVel = {angVel}")
+        for point in self.points:
+            point.printPoint()
+        self.cm.printPoint()
+    def updateCorners(self):
+        for i in self.points:
+            i.updatePoint(self.angDispl, self.cm)
+    def updateCM(self, dt):
+        self.cm.updatePoint(dt)
+    def updateCMVelocity(self, g, dt):
+        self.cm.updateNoCollisionVelocity(g, dt)
+    def plotPoints(self):
+        pointsx = [self.points[0].x,self.points[1].x,self.points[2].x,self.points[0].x]
+        pointsy = [self.points[0].y,self.points[1].y,self.points[2].y,self.points[0].y]
+        plt.plot(pointsx, pointsy)    
+        print("X: ", pointsx)
+        print("Y: ", pointsy)
+    def getPointsX(self):
+        return [self.points[0].x,self.points[1].x,self.points[2].x,self.points[0].x]
+    def getPointsY(self):
+        return [self.points[0].y,self.points[1].y,self.points[2].y,self.points[0].y]
+        
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def updatePoint(self, angDispl, cm):
+        self.x = self.x*math.cos(angDispl) - self.y*math.sin(angDispl) + cm.x
+        self.y = self.x*math.sin(angDispl) + self.y*math.cos(angDispl) + cm.y
+        return self
+    def printPoint(self):
+        print(f"Location: {self.x}, {self.y}")
+    def plotPoint(self):
+        plt.plot(self.x, self.y)
+    
+class CenterOfMass(Point):
+    def __init__(self,x,y, vx, vy):
+        super().__init__(x,y)
+        self.vx = vx
+        self.vy = vy
+    def printPoint(self):
+        super().printPoint()
+        print(f"Velocity: {self.vx}i + {self.vy}j")
+    def updatePoint(self, dt):
+        self.x = self.x + self.vx * dt
+        self.y = self.y + self.vy * dt
+        #self.printPoint()
+        return self
+    def updateNoCollisionVelocity(self, g, dt):
+        self.vy = self.vy - g * dt
 
 # reference points introduced
 
@@ -315,12 +374,14 @@ while (time < 3.0):
             VycmA = -VycmA
             wA = list(wA)
             wA[2] += J/IA * crossrn[2]
+    C.updateCMVelocity(g, dt)
 
     # calculating and updating the center of mass
     cmAx = cmAx + VxcmA * dt
     cmAy = cmAy + VycmA * dt
     dotsAx.append(dotsAx[-1] + VxcmA * dt)
     dotsAy.append(dotsAy[-1] + VycmA * dt)
+    
     
     # calculating and updating the corners
     Ax[0] = p1Ai[0]*math.cos(yA) - p1Ai[1]*math.sin(yA) + cmAx
@@ -333,6 +394,7 @@ while (time < 3.0):
     Ay[2] = p3Ai[0]*math.sin(yA) + p3Ai[1]*math.cos(yA) + cmAy
     Ay[3] = Ay[0]
     
+    
     plt.plot(Ax, Ay)
     
     # A gets updated
@@ -340,6 +402,13 @@ while (time < 3.0):
     A2 = (Ax[1], Ay[1])
     A3 = (Ax[2], Ay[2])
     A = (A1, A2, A3, A1)
+    
+    C.updateCM(dt)
+    C.updateCorners()
+    #C.plotPoints()
+    CX = C.getPointsX()
+    CY = C.getPointsY()
+    plt.plot(CX, CY)    
     
     # Same for triangle B
     if(collA!=True and collB!=True):
